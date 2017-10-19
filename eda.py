15cb87688c91ce808b40abdcc2df1a1ec2f3d8ee
Ronald Macmaster <ronnymacmaster@gmail.com>
2017-10-18 20:44:18 -0500

    random forest classifier on a few features. 0.70 test error

--- eda.py
+++ eda.py
@@ -13,52 +13,83 @@ import scipy as sp
 mpl.style.use(["fivethirtyeight"])
 sns.set(style="whitegrid", color_codes=True)
 
-data = pd.read_csv("train.csv", index_col="id").dropna()
+train = pd.read_csv("train.csv", index_col="id")
+data = train.drop(["Y"], axis=1)
+y = train["Y"]
+
+# drop some collinear features
 data.drop(["F23", "F26"], axis=1, inplace=True)
-print data.head()
-print data.describe()
 
-# PLOTDIR = "plots/univariate"
-# for feat in data:
-#     print "univariate: ", feat
-#     sns.distplot(data[feat].dropna(), kde=False)
-#     plt.title(feat)
-#     plt.savefig("%s/%s.png" % (PLOTDIR, feat))
-#     plt.close()
-
-# skew experiments
-from sklearn.preprocessing import scale
+# unskew some features with boxcox
 from scipy.stats import skew, boxcox
-PLOTDIR = "plots/unibox"
-# for feat in data[["F5", "F6", "F9", "F10", "F16", "F20", "F21"]]:
-for feat in data[["F3", "F19", "F22", "F27"]]:
-    skewness = stats.skew(data[feat])
-    skewedData = scale(data[feat])
-    unskewedData = scale(boxcox(data[feat] + 1)[0])
-    # unskewedData = scale(np.log1p(data[feat]))
-    
-    # histogram
-    plt.subplot(3, 1, 1)
-    plt.hist(skewedData, bins=500, facecolor="#440055", alpha=0.75)
-    plt.title(feat + " histogram")
-    plt.annotate("skewness: {0:.2f}".format(skew(skewedData)), 
-        xy=(0.8, 0.8), xycoords="axes fraction")
+from sklearn.preprocessing import scale
+# data["F3"] = boxcox(data["F3"].fillna(0) + 1)[0]
+# data["F19"] = boxcox(data["F19"].fillna(0) + 1)[0]
+# data["F22"] = boxcox(data["F22"].fillna(0) + 1)[0]
+# data["F27"] = boxcox(data["F27"].fillna(0) + 1)[0]
 
-    # unskewed histogram
-    plt.subplot(3, 1, 2)
-    plt.hist(unskewedData, bins=500, facecolor="#440055", alpha=0.75)
-    plt.title(feat + " unskewed histogram")
-    plt.annotate("skewness: {0:.2f}".format(skew(unskewedData)), 
-        xy=(0.8, 0.8), xycoords="axes fraction")
- 
-    # boxplot
-    plt.subplot(3, 1, 3)
-    plt.boxplot(unskewedData)
-    plt.title(feat + " boxplot")
+# preprocessing pipeline
+print "data before pipeline: ", data.shape
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.preprocessing import Imputer, StandardScaler, RobustScaler
+columns = data.columns
+pipeline = Pipeline([
+    ("imputer", Imputer(strategy="median")),
+    # ("scaler", RobustScaler()),
+])
 
-    plt.tight_layout()
+print data.columns
+data = pipeline.fit_transform(data)
+data = pd.DataFrame(data, columns=columns)
+
+PLOTDIR = "plots/univariate"
+# for feat in data[["F6", "F9", "F16", "F21"]]:
+for feat in data:
+    print "univariate: ", feat, data[feat].dtype
+    print data[feat].describe()
+    sns.distplot(data[feat], bins=50, kde=False)
+    plt.title(feat)
+    # plt.xlim(0, 1000)
+    # plt.savefig("%s/%s.png" % (PLOTDIR, feat))
     plt.show()
     plt.close()
+
+# # skew experiments
+# from sklearn.preprocessing import scale
+# from scipy.stats import skew, boxcox
+# PLOTDIR = "plots/unibox"
+# # feats = ["F5", "F6", "F9", "F10", "F16", "F20", "F21"]
+# # feats = ["F3", "F19", "F22", "F27"]
+# feats = data.columns
+# for feat in data[feats]:
+#     skewness = stats.skew(data[feat])
+#     skewedData = scale(data[feat])
+#     unskewedData = scale(boxcox(data[feat] + 1)[0])
+#     # unskewedData = scale(np.log1p(data[feat]))
+#     data["unskewed" + feat] = unskewedData
+#     
+#     # histogram
+#     plt.subplot(3, 1, 1)
+#     plt.hist(skewedData, bins=500, facecolor="#440055", alpha=0.75)
+#     plt.title(feat + " histogram")
+#     plt.annotate("skewness: {0:.2f}".format(skew(skewedData)), 
+#         xy=(0.8, 0.8), xycoords="axes fraction")
+# 
+#     # unskewed histogram
+#     plt.subplot(3, 1, 2)
+#     plt.hist(unskewedData, bins=500, facecolor="#440055", alpha=0.75)
+#     plt.title(feat + " unskewed histogram")
+#     plt.annotate("skewness: {0:.2f}".format(skew(unskewedData)), 
+#         xy=(0.8, 0.8), xycoords="axes fraction")
+# 
+#     # boxplot
+#     plt.subplot(3, 1, 3)
+#     plt.boxplot(unskewedData)
+#     plt.title(feat + " boxplot")
+# 
+#     plt.tight_layout()
+#     plt.show()
+#     plt.close()
 
 
 # PLOTDIR = "plots/bivariate"
@@ -71,13 +102,13 @@ for feat in data[["F3", "F19", "F22", "F27"]]:
 #     plt.close()
 
 # PLOTDIR = "plots"
-# feats = ["F2", "F3", "F5", "F14", "F18", "F23", "F25", "F26", "Y"]
-# subset = data # data[feats]
-# ax = sns.heatmap(subset.corr(), vmin=-0.7, vmax=0.7, )# annot=True)
+# feats = ["F2", "F3", "F5", "F10", "F14", "F18", "F25", "Y", "unskewedF3", "unskewedF19", "unskewedF22", "unskewedF27"]
+# subset = data[feats]
+# ax = sns.heatmap(subset.corr(), vmin=-0.7, vmax=0.7, annot=True)
 # ax.set_title("Correlation Heatmap")
 # ax.set_yticklabels(ax.get_yticklabels(), rotation=0)
 # ax.set_xticklabels(ax.get_xticklabels(), rotation=90)
 # plt.savefig("%s/%s.png" % (PLOTDIR, "heatmap"))
 # plt.show()
 # plt.close()
-
+# 
--- model.py
+++ model.py
@@ -30,10 +30,10 @@ data.drop(["Y"], axis=1, inplace=True)
 # unskew some features with boxcox
 from scipy.stats import skew, boxcox
 from sklearn.preprocessing import scale
-data["F3"] = scale(boxcox(data["F3"].fillna(data["F3"].mean()) + 1)[0])
-data["F19"] = scale(boxcox(data["F19"].fillna(data["F19"].mean()) + 1)[0])
-data["F22"] = scale(boxcox(data["F22"].fillna(data["F22"].mean()) + 1)[0])
-data["F27"] = scale(boxcox(data["F27"].fillna(data["F27"].mean()) + 1)[0])
+data["F3"] = scale(boxcox(data["F3"].fillna(0) + 1)[0])
+data["F19"] = scale(boxcox(data["F19"].fillna(0) + 1)[0])
+data["F22"] = scale(boxcox(data["F22"].fillna(0) + 1)[0])
+data["F27"] = scale(boxcox(data["F27"].fillna(0) + 1)[0])
 
 # drop some collinear features
 data.drop(["F23", "F26"], axis=1, inplace=True)
@@ -45,7 +45,7 @@ from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.preprocessing import Imputer, StandardScaler, RobustScaler
 columns = data.columns
 pipeline = Pipeline([
-    ("imputer", Imputer(strategy="mean")),
+    ("imputer", Imputer(strategy="median")),
     ("scaler", RobustScaler()),
 ])
 
@@ -68,23 +68,40 @@ ytrain = train["Y"]
 
 # train a simple classifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import auc, roc_curve
-clf = LogisticRegression()
-clf.fit(xtrain, ytrain)
-clf_pred = clf.decision_function(xtrain)
+lr_clf = LogisticRegression()
+lr_clf.fit(xtrain, ytrain)
+lr_clf_pred = lr_clf.predict(xtrain)
 
-fpr, tpr, _ = roc_curve(ytrain, clf_pred)
+# train a random forest 
+from sklearn.ensemble import RandomForestClassifier
+forest_clf = RandomForestClassifier(max_features=None)
+forest_clf.fit(xtrain, ytrain)
+forest_clf_pred = forest_clf.predict(xtrain)
+
+print "Feature Importances(%s):\n" % xtrain.columns.values, forest_clf.feature_importances_
+
+# evaluate performance metrics
+from sklearn.metrics import auc, roc_curve, confusion_matrix
+clf = forest_clf # pick a classifier
+clf_pred = clf.predict(xtrain) 
+print "confusion_matrix:\n", confusion_matrix(ytrain, clf_pred)
+fpr, tpr, thresholds = roc_curve(ytrain, clf_pred)
 roc_auc = auc(fpr, tpr)
+
+# plot roc curve
 plt.plot(fpr, tpr, color="r", label="ROC Curve (area = %0.2f)" % roc_auc)
 plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.0])
 plt.legend(loc="lower right")
+plt.title("Receiver Operating Curve")
+plt.ylabel("True Positive Rate")
+plt.xlabel("False Positive Rate")
+plt.tight_layout()
 plt.show()
 plt.close()
 
-
-# # submit solution
-# submit = pd.DataFrame()
-# submit["id"] = test.index
-# submit["Y"] = 0.0
-# submit.to_csv("submit.csv", index=False)
+# submit solution
+submit = pd.DataFrame()
+submit["id"] = test.index
+submit["Y"] = clf.predict_proba(xtest)[:, 1]
+submit.to_csv("submit.csv", index=False)
